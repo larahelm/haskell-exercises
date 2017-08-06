2ab4ef5d9e07c901cf9d3ab41502aecff1281439
larahelm <lara.helm@gmail.com>
2017-08-06 03:09:50 -0400

    Update Pairs.hs

--- Pairs.hs
+++ Pairs.hs
@@ -1,12 +1,12 @@
--- Pairs.hs
+-- pairs.hs
 module Pairs where
 pairs :: String -> [[Char]]
 pairs s = 
-    if length s <= 2
+    if length s <= 1
         then 
             if length s == 1
                 then [s ++ "_"]
             else 
-                [s]
+                []
     else
         [take 2 s] ++ (pairs $ drop 2 s)
